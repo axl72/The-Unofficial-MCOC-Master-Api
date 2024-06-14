21d497fa30f0dd3afd1381c31e2e4b4124f067fc
axl72 <axell.bernabel@unmsm.edu.pe>
2024-06-14 00:15:23 -0500

    add server start

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
-import express from 'express'
+import { Server } from "./Server";
+import { ServerOptions } from "./Server";
 
-const app = express()
+const options:ServerOptions = {
+
+}
+
+const server = new Server(options);
+server.start();
