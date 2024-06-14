a32afb7b3379d41a2ab884ac2c617682cf4b9582
axl72 <axell.bernabel@unmsm.edu.pe>
2024-06-14 15:11:34 -0500

    implemented jsonwebtoken

---
+++ src/services/jwtoken.service.ts
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken'
+
+export class JwToken {
+    static async generateToken(payload:any, duration:string='2h') {
+        return new Promise((resolve) => {
+            jwt.sign(payload, "SEED", {expiresIn: duration}, (err, token) => {
+                if(err) return resolve(null);
+
+                resolve(token)
+            })
+        })
+    }
+}
