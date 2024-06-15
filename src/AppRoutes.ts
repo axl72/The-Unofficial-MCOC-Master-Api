1c6322427b4d6f71f12e763384bec8287872c467
axl72 <axell.bernabel@unmsm.edu.pe>
2024-06-15 13:21:47 -0500

    add new routes to server

--- src/AppRoutes.ts
+++ src/AppRoutes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { ChampionsRoutes } from "./Routes/ChampionsRoutes";
+import { ChampionsRoutes } from "./routes/ChampionsRoutes";
 import 'dotenv/config'
+import { LoginRoutes } from "./routes/LoginRoutes";
+import { RegisterRoutes } from "./routes/RegisterRoutes"
 
 export class AppRoutes {
     static get routes(): Router {
         const router = Router();
 
         router.use('/api/champions', ChampionsRoutes.routes)
+        router.use('/login', LoginRoutes.routes)
+        router.use('/register', RegisterRoutes.routes)
 
         return router;
     }
