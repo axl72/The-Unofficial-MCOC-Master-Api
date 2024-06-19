5acf680076847788e7dc953f1beba55a8d4d9478
axl72 <axell.bernabel@unmsm.edu.pe>
2024-06-18 23:35:15 -0500

    add barrel file

--- src/AppRoutes.ts
+++ src/AppRoutes.ts
@@ -1,16 +1,14 @@
 import { Router } from "express";
-import { ChampionsRoutes } from "./routes/ChampionsRoutes";
 import 'dotenv/config'
-import { LoginRoutes } from "./routes/LoginRoutes";
-import { RegisterRoutes } from "./routes/RegisterRoutes"
+import { Champions, Login, Register } from './routes'
 
 export class AppRoutes {
     static get routes(): Router {
         const router = Router();
 
-        router.use('/api/champions', ChampionsRoutes.routes)
-        router.use('/login', LoginRoutes.routes)
-        router.use('/register', RegisterRoutes.routes)
+        router.use('/api/champions', Champions.routes)
+        router.use('/login', Login.routes)
+        router.use('/register', Register.routes)
 
         return router;
     }
