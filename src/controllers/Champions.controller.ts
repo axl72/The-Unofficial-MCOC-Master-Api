5315b146405b864806eae2888e5e37b4d6892e4d
axl72 <axell.bernabel@unmsm.edu.pe>
2024-06-26 01:12:01 -0500

    fix storage database (todo: modified read and create controller methods)

--- src/controllers/Champions.controller.ts
+++ src/controllers/Champions.controller.ts
@@ -3,6 +3,20 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
+interface Champion {
+    name: string;
+}
+
+interface Bonus {
+    description: string;
+}
+
+interface Synergy {
+    name: string;
+    champions: Champion[];
+    bonuses: Bonus[];
+}
+
 export class ChampionsController {
     public getChampionsByName = async(req:Request, res:Response) => {
         const {name} = req.body
@@ -12,6 +26,59 @@ export class ChampionsController {
                 {
                     where: {
                         name: name       
+                    },
+                    include: {
+                        skills: {
+                            select: {
+                                title: true,
+                                description: true
+                            }
+                        },
+                        attributes: {
+                            select: {
+                                armor_penetration: true,
+                                armor_raiting: true,
+                                block_penetration: true,
+                                block_proficiency: true,
+                                critical_damage_raiting: true,
+                                critical_raiting: true,
+                                critical_resistence: true,
+                                energy_resistence: true,
+                                details: true
+                                
+                            }
+                        }
+                        ,
+                        inmunities: {
+                            select: {
+                                name: true
+                            }
+                        },
+                        resistences: {
+                            select: {
+                                inmunity_asociated: true,
+                                description: true
+                            }
+                        },
+                        tags: {
+                            select: {
+                                name: true
+                            }
+                        },
+                        signatureAbility: {
+                            select: {
+                                name: true,
+                                description: true
+                            }
+                        },
+                        specialAttacks: true,
+                        synergies: {
+                            include: {
+                                champions: true,
+                                bonuses: true
+                            }
+                        },
+                        sprites: true
                     }
                 }
             )
@@ -24,20 +91,47 @@ export class ChampionsController {
 
     public createChampion = async(req:Request, res:Response) => {
         const {name, biography, releaseDate, skills, attributes, inmunities, resistences, tags, signatureAbility, specialAttacks, synergies, sprites} = req.body
-        console.log(resistences)
+        console.log(synergies.name)
         try{
             await prisma.champion.create({data: {
                 name: name,
                 biography: biography,
                 releaseDate: new Date(releaseDate),
-                skills,
-                attributes,
-                inmunities,
-                resistences,
-                tags,
-                signatureAbility,
-                specialAttacks,
-                sprites: sprites,
+                skills: {
+                    create: skills
+                },
+                attributes: {
+                    create: attributes
+                },
+                inmunities: {
+                    create: inmunities.create
+                },
+                resistences: {
+                    create: resistences
+                },
+                tags: {
+                    create: tags
+                },
+                signatureAbility: {
+                    create: signatureAbility
+                },
+                specialAttacks: {
+                    create: specialAttacks
+                },
+                sprites: {
+                    create: sprites
+                },
+                synergies: {
+                    create: synergies.map((synergy: Synergy) => ({
+                        name: synergy.name,
+                        champions: {
+                            create: synergy.champions
+                        },
+                        bonuses: {
+                            create: synergy.bonuses
+                        }
+                    }))
+                }
 
 
             }});
