99d2f742e3395cfaf2cbfdf0b67caef829a397bb
axl72 <axell.bernabel@unmsm.edu.pe>
2024-06-28 03:10:06 -0500

    add cors middleware

--- src/Server.ts
+++ src/Server.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, Router } from 'express'
 import { AppRoutes } from './AppRoutes';
+import cors from 'cors'
 
 export interface ServerOptions  {
     PORT: any
@@ -18,6 +19,7 @@ export class Server {
 
     public start():void {
         this.app.use(express.json())
+        this.app.use(cors());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(this.routes);
         this.app.listen(this.opts.PORT, () => {
