e9de793cdca67c3210aa0e2d721775646c745fe4
axl72 <axell.bernabel@unmsm.edu.pe>
2024-06-28 03:21:06 -0500

    implemented validateToken function

--- src/services/jwtoken.service.ts
+++ src/services/jwtoken.service.ts
@@ -1,12 +1,22 @@
 import jwt from 'jsonwebtoken'
+import { envs } from './envs.service';
 
 export class JwToken {
     static async generateToken(payload:any, duration:string='2h') {
         return new Promise((resolve) => {
-            jwt.sign(payload, "SEED", {expiresIn: duration}, (err, token) => {
-                if(err) return resolve(null);
-
+            jwt.sign(payload, envs.TOKEN_SECRET_KEY!, {expiresIn: duration}, (err, token) => {
+                if(err) {
+                    return resolve(null);
+                }
                 resolve(token)
+            })
+        })
+    }
+
+    static async validateToken<T>(token: string): Promise<T> {
+        return new Promise((resolve) => {
+            jwt.verify(token, envs.TOKEN_SECRET_KEY!, (err, decoded) => {
+                resolve(decoded as T)
             })
         })
     }
