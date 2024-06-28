b8fb3975e00cadb228d235dd458e65fc80b81e57
axl72 <axell.bernabel@unmsm.edu.pe>
2024-06-28 03:12:44 -0500

    refactoring code, use model and dto implementation class

--- src/controllers/Champions.controller.ts
+++ src/controllers/Champions.controller.ts
@@ -1,35 +1,25 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import { ChampionDTO } from "../model/champion.dto";
+import { Champion } from "../model";
 
 const prisma = new PrismaClient();
 
-interface Champion {
-  name: string;
-}
-
-interface Bonus {
-  description: string;
-}
-
-interface Synergy {
-  name: string;
-  champions: Champion[];
-  bonuses: Bonus[];
-}
-
 export class ChampionsController {
   public getChampionsByName = async (req: Request, res: Response) => {
-    const { name } = req.body;
+    const championName =  req.params.championName;
+
     try {
-      const champion = await ChampionDTO.findChampionByName(name);
+      const champion = await Champion.findChampionByName(championName);
+
+      if(!champion) return res.json({message: "Champion not found"})
+
       res.json(champion);
     } catch (error) {}
   };
 
   public createChampion = async (req: Request, res: Response) => {
     try {
-      ChampionDTO.create(req.body);
+      Champion.create(req.body);
       res.json({ message: "Champion created successful" });
     } catch (error) {
       console.log(error);
