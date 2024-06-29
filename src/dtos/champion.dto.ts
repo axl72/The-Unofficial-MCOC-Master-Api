cabe541d45cdbb84a92ff19899ff110df02339a1
axl72 <axell.bernabel@unmsm.edu.pe>
2024-06-29 03:06:05 -0500

    implemented champion dto

--- src/dtos/champion.dto.ts
+++ src/dtos/champion.dto.ts
@@ -56,4 +56,82 @@ export class ChampionDTO {
       sprites
     })
   };
+
+  static createFromObjectDatabase = (object: {[key: string]: any}) => {
+     const {
+      name,
+      biography,
+      abilities,
+      attributes,
+      inmunities,
+      releaseDate,
+      resistences,
+      signatureAbility,
+      specialAttacks,
+      sprites,
+      synergies,
+      tags,
+    } = object ?? {};
+    const { id, championId, details, ...attributesResult } = attributes ?? {};
+    const { id: detailsId, ...moreDetails } = details ?? {};
+
+    const championResult = {
+      name,
+      biography,
+      releaseDate,
+      attributes: {
+        ...attributesResult,
+        details: { ...moreDetails },
+      },
+      inmunities: inmunities?.map((inmunity:any) => inmunity),
+      resistences: resistences?.map(({ inmunity_asociated, ratio }: any) => ({
+        inmunity_asociated,
+        ratio,
+      })),
+      signatureAbility: {
+        name: signatureAbility?.name,
+        type: signatureAbility?.type,
+        bonuses: signatureAbility?.bonuses.map(
+          ({ description}:any) => description
+        ),
+      },
+      specialAttacks: specialAttacks?.map(({ name, description, bonuses }:any) => ({
+        name,
+        description,
+        bonuses: bonuses.map(({ description }:any) => description),
+      })),
+      synergies: synergies?.map(({ name, champions, bonuses }:any) => ({
+        name,
+        bonuses: bonuses.map(({ description }:any) => description),
+        champions: champions.map(({ name }:any) => ({
+          name,
+        })),
+      })),
+      tags: {
+        releaseDate: tags?.releaseDate,
+        allianceQuest: tags?.allianceQuest.map(
+          ({ description }:any) => description
+        ),
+        allianceWars: tags?.allianceWars.map(({ description }:any) => description),
+        attributes: tags?.attributes.map(({ description }:any) => description),
+        carinasChallenges: tags?.carinasChallenges.map(
+          ({ description }:any) => description
+        ),
+        combatStyle: tags?.combatStyle.map(({ description }:any) => description),
+        organization: tags?.organization.map(({ description }:any) => description),
+        saga: tags?.saga,
+      },
+
+      sprites: sprites?.map(({ source, url }:any) => ({
+        source,
+        url,
+      })),
+      abilities: abilities?.map(({ title, characteristics }:any) => ({
+        title,
+        characteristics: characteristics.map(({ description }:any) => description),
+      })),
+    };
+
+    return new ChampionDTO(championResult);
+  }
 }
