b8d4efc322b063cb32c8ecd6c017eeaa384eb6cc
axl72 <axell.bernabel@unmsm.edu.pe>
2024-07-08 13:36:36 -0500

    add todo commment

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -4,7 +4,7 @@ import { JwToken } from "../services/jwtoken.service";
 export class LoginController {
   public sendToken = (req: Request, res: Response) => {
     const { user, password } = req.body;
-    if (user === "axell" && password === "1234") // comprobar autenticación
+    if (user === "axell" && password === "1234") // Todo: comprobar autenticación
       JwToken.generateToken({user}).then((token) => {
         res.json({token});
       });
