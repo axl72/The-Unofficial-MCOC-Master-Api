3d078047743af78e4357ff4f9d0dae39b6c198e2
axl72 <axell.bernabel@unmsm.edu.pe>
2024-08-20 15:55:06 -0500

    implemented new methods

--- src/dtos/champion.dto.ts
+++ src/dtos/champion.dto.ts
@@ -24,6 +24,136 @@ export class ChampionDTO {
     return this.dataChampion;
   }
 
+  static createChampionFromObject = (object: {[key: string]: any}) => {
+    const {
+      name,
+      biography,
+      releaseDate,
+      abilities,
+      attributes,
+      inmunities,
+      resistences,
+      tags,
+      signatureAbility,
+      specialAttacks,
+      synergies,
+      sprites,
+    } = object;
+
+    const champion = {
+      data: {
+        name: name,
+        biography: biography,
+        releaseDate: new Date(releaseDate),
+        abilities: {
+          create: abilities.map((ability: any) => ({
+            title: ability.title,
+            characteristics: {
+              create: ability.characteristics.map((characteristic: any) => ({
+                description: characteristic,
+              })),
+            },
+          })),
+        },
+        attributes: {
+          create: {
+            armor_penetration: attributes.armor_penetration,
+            armor_raiting: attributes.armor_raiting,
+            block_penetration: attributes.block_penetration,
+            block_proficiency: attributes.block_proficiency,
+            critical_damage_raiting: attributes.critical_damage_raiting,
+            critical_raiting: attributes.critical_raiting,
+            critical_resistence: attributes.critical_resistence,
+            energy_resistence: attributes.energy_resistence,
+            details: {
+              create: attributes.details,
+            },
+          },
+        },
+        inmunities: {
+          create: inmunities.create,
+        },
+        resistences: {
+          create: resistences,
+        },
+        tags: {
+          create: {
+            releaseDate: tags.releaseDate,
+            saga: tags.saga,
+            allianceQuest: {
+              create: tags.allianceQuest.map((tag: any) => ({
+                description: tag,
+              })),
+            },
+            allianceWars: {
+              create: tags.allianceWars.map((tag: any) => ({
+                description: tag,
+              })),
+            },
+            attributes: {
+              create: tags.attributes.map((tag: any) => ({
+                description: tag,
+              })),
+            },
+            carinasChallenges: {
+              create: tags.carinasChallenges.map((tag: any) => ({
+                description: tag,
+              })),
+            },
+            combatStyle: {
+              create: tags.combatStyle.map((tag: any) => ({
+                description: tag,
+              })),
+            },
+          },
+        },
+        signatureAbility: {
+          create: {
+            name: signatureAbility.name,
+            type: signatureAbility.type,
+            bonuses: {
+              create: signatureAbility.bonuses.map((bonus: string) => ({
+                description: bonus,
+              })),
+            },
+          },
+        },
+        specialAttacks: {
+          create: specialAttacks.map((specialAttack: any) => ({
+            name: specialAttack.name,
+            description: specialAttack.description,
+          })),
+        },
+        sprites: {
+          create: sprites,
+        },
+        synergies: {
+          create: synergies.map((synergy: any) => ({
+            name: synergy.name,
+            champions: {
+              create: synergy.champions.map((champion: any) => ({
+                name: champion.name,
+                bonuses: {
+                  create: champion.bonuses.map((bonus: String) => ({
+                    description: bonus,
+                  })),
+                },
+              })),
+            },
+            bonuses: {
+              create: synergy.bonuses,
+            },
+          })),
+        },
+      },
+    }
+    return champion
+  }
+
+  static createChampionFromObjectoToUpdate = (object: {[key: string]: any}) => {
+    
+  }
+
 
   static createFromObjectRequest = (object: { [key: string]: any }) => {
     const {
